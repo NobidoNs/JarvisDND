ae0f33ceb93facf9075e54642689fb4486159968
nobidons <nobidon.tim@gmail.com>
2025-06-17 11:05:44 +0500

    1.0

--- temp.py
+++ temp.py
@@ -1,10 +1,20 @@
+# from g4f.client import Client
+
+# client = Client()
+# response = client.images.generate(
+#     model="flux",
+#     prompt="a white siamese cat",
+#     response_format="url"
+# )
+
+# print(f"Generated image URL: {response.data[0].url}")
+
 from g4f.client import Client
 
 client = Client()
-response = client.images.generate(
-    model="flux",
-    prompt="a white siamese cat",
-    response_format="url"
+response = client.chat.completions.create(
+    model="gpt-4o",
+    messages=[{"role": "user", "content": "Hello"}],
+    web_search=False
 )
-
-print(f"Generated image URL: {response.data[0].url}")
+print(response.choices[0].message.content)
