77d3124e044ba79fbe6f0bf9eb649e98a7097ce5
NobidoNs <61043287+NobidoNs@users.noreply.github.com>
2025-06-26 22:14:03 +0500

    4 images generate

--- services/image_service.py
+++ services/image_service.py
@@ -7,27 +7,32 @@ class ImageService:
         self.client = Client()
     
     def generate_image(self, user_id, prompt):
-        """Generate image from prompt"""
+        """Generate 4 images from prompt"""
         try:
             enhanced_prompt = self._enhance_prompt(prompt)
             
-            response = self.client.images.generate(
-                model="sdxl-1.0",
-                prompt=enhanced_prompt,
-                response_format="url"
-            )
-
-            # Save generated image to database
-            image = GeneratedImage(
-                url=response.data[0].url,
-                prompt=enhanced_prompt,
-                user_id=user_id,
-                source='image_generator'
-            )
-            db.session.add(image)
+            # Generate 4 images using separate requests
+            image_urls = []
+            for i in range(4):
+                response = self.client.images.generate(
+                    model="sdxl-1.0",
+                    prompt=enhanced_prompt,
+                    response_format="url"
+                )
+                
+                # Save generated image to database
+                image = GeneratedImage(
+                    url=response.data[0].url,
+                    prompt=enhanced_prompt,
+                    user_id=user_id,
+                    source='image_generator'
+                )
+                db.session.add(image)
+                image_urls.append(response.data[0].url)
+            
             db.session.commit()
             
-            return {"image_url": response.data[0].url}
+            return {"image_urls": image_urls}
             
         except Exception as e:
             print(f"Error generating image: {str(e)}")
